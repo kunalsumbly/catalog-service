f5fb74ca2309f97220d7b22b8f551f50306a8620
Kunal Sumbly <kunal.sumbly@optus.com.au>
2025-07-16 16:31:15 +1000

    working config client with encrypt.key and encrypt.salt coming from secrets manager

--- src/main/java/com/polarbookshop/catalogservice/config/client/ClientDecryptionConfiguration.java
+++ src/main/java/com/polarbookshop/catalogservice/config/client/ClientDecryptionConfiguration.java
@@ -1,27 +1,24 @@
 package com.polarbookshop.catalogservice.config.client;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 @Slf4j
 public class ClientDecryptionConfiguration {
 
-    @Value("${encrypt.key}")
-    private String encryptionKey;
-
-    @Value("${encrypt.salt}")
-    private String encryptionSalt;
-
     @Bean
     @Primary
-    public TextEncryptor textEncryptor() {
-        log.info("Configuring client-side decryption with matching encryption parameters");
+    public TextEncryptor textEncryptor(
+            @Value("${encrypt.key}") String encryptionKey,
+            @Value("${encrypt.salt}") String encryptionSalt) {
+
+        log.info("Configuring client-side decryption with key: {} and salt: {}", encryptionKey, encryptionSalt);
         return Encryptors.text(encryptionKey, encryptionSalt);
     }
 }
