a098879661d15f23d618843a6139f4a0fd835782
Kunal Sumbly <kunal.sumbly@optus.com.au>
2025-08-15 16:26:34 +1000

    timeout things

--- src/main/java/com/polarbookshop/catalogservice/config/client/RestConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/client/RestConfig.java
@@ -2,6 +2,7 @@ package com.polarbookshop.catalogservice.config.client;
 
 import lombok.Data;
 import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
@@ -76,12 +77,20 @@ public class RestConfig {
                 .build();
 
         // Connection timeout configuration
+        ConnectionConfig connectionConfig = ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.ofMilliseconds(connectTimeoutMs))
+                .build();
+
+        // RequestConfig now only handles request-level timeouts
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(Timeout.ofMilliseconds(connectTimeoutMs))
                 .build();
 
+
+
         HttpClientConnectionManager httpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder.create()
                 .setDefaultSocketConfig(socketConfig)
+                .setDefaultConnectionConfig(connectionConfig)  // Apply ConnectionConfig here
                 .setMaxConnPerRoute(20)
                 .setMaxConnTotal(20)
                 .setSSLSocketFactory(connectionFactory)
