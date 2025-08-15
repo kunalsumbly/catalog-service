52b75b2f60d218c508a6f536103cb0ee50d7d5ac
Kunal Sumbly <kunal.sumbly@optus.com.au>
2025-08-15 11:44:48 +1000

    timeout things

--- src/main/java/com/polarbookshop/catalogservice/domain/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.catalogservice.domain.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String isbn) {
-        super("The book with ISBN " + isbn + " was not found.");
-    }
-}
