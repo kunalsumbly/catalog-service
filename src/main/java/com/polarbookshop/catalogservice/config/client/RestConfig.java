dc2d1944801ac4d25a0fbdde9e1ab8129888b10b
Kunal Sumbly <kunal.sumbly@optus.com.au>
2025-08-15 15:14:11 +1000

    Revert "timeout things"
    This reverts commit 7bfba4f0328f37a5bf05ca1ae23b67abb510cba1.

--- src/main/java/com/polarbookshop/catalogservice/config/client/RestConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/client/RestConfig.java
@@ -2,7 +2,6 @@ package com.polarbookshop.catalogservice.config.client;
 
 import lombok.Data;
 import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.io.HttpClientConnectionManager;
@@ -75,12 +74,6 @@ public class RestConfig {
                 .setSoTimeout(Timeout.ofMilliseconds(readTimeoutMs))
                 .build();
 
-        // Connection timeout configuration
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(Timeout.ofMilliseconds(connectTimeoutMs))
-                .setConnectionRequestTimeout(Timeout.ofMilliseconds(connectTimeoutMs))
-                .build();
-
         HttpClientConnectionManager httpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder.create()
                 .setDefaultSocketConfig(socketConfig)
                 .setMaxConnPerRoute(20)
@@ -89,8 +82,6 @@ public class RestConfig {
                 .build();
 
         return HttpClients.custom()
-                .setConnectionManager(httpClientConnectionManager)
-                .setDefaultRequestConfig(requestConfig)
-                .build();
+                .setConnectionManager(httpClientConnectionManager).build();
     }
 }
